f79d0005bc0f1bd43b1d735948226b58070ec11b
kwonjeong <kwonjeong9470@gmail.com>
2023-02-17 21:07:43 +0900

    [Feat] #3 - 이미지 에셋 추가

---
+++ Kakao-Clone/Kakao-Clone/Presentation/Main/FriendViewController.swift
@@ -0,0 +1,51 @@
+//
+//  FriendViewController.swift
+//  Kakao-Clone
+//
+//  Created by KJ on 2023/02/13.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class FriendViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    private let friendLabel = UILabel()
+    private let settingImage = UIImage()
+    private let myProfileView = UIView()
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+extension FriendViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
---
+++ Kakao-Clone/Kakao-Clone/Util/NameSpace/Image.swift
@@ -0,0 +1,23 @@
+//
+//  Image.swift
+//  Kakao-Clone
+//
+//  Created by KJ on 2023/02/17.
+//
+
+import UIKit
+
+public enum Image {
+    
+    // MARK: - FriendView
+    
+    public static let profileImage = UIImage(named: "profileImg")!
+    public static let settingIcon = UIImage(named: "settingIc")!
+    
+    // MARK: - MyProfileView
+    
+    public static let profileEditing = UIImage(named: "profileEditImg")!
+    public static let profileStory = UIImage(named: "profileStoryImg")!
+    public static let profileTalk = UIImage(named: "profileTalkImg")!
+    
+}
