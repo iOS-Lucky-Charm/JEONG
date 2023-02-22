23c135d9121dc64003d8e0dc03d8385785acdc8d
kwonjeong <kwonjeong9470@gmail.com>
2023-02-23 01:21:25 +0900

    [feat] #8 - underLine 색상 변화 구현 완료

--- Kakao-Clone/Kakao-Clone/Presentation/Authentication/SignIn/View/SignInTextFieldView.swift
+++ Kakao-Clone/Kakao-Clone/Presentation/Authentication/SignIn/View/SignInTextFieldView.swift
@@ -91,6 +91,24 @@ extension SignInTextFieldView {
 extension SignInTextFieldView: UITextFieldDelegate {
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-            signTextField.textColor = .black
+        signTextField.textColor = .black
+    }
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        underLineView.backgroundColor = .black
+        return true
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        underLineView.backgroundColor = .black
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        let text = signTextField.text
+        if text == "" {
+            underLineView.backgroundColor = .systemGray3
+        }
+        return true
     }
 }
