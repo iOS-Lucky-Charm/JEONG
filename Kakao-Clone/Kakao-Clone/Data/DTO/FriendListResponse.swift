8db8ed3e88b74212e484dde8e70e1fae1672cb62
kwonjeong <kwonjeong9470@gmail.com>
2023-03-28 16:48:13 +0900

    [Feat] #16 - FriendListResponse 생성

--- Kakao-Clone/Kakao-Clone/Data/DTO/FriendListResponse.swift
+++ Kakao-Clone/Kakao-Clone/Data/DTO/FriendListResponse.swift
@@ -5,14 +5,29 @@
 //  Created by KJ on 2023/03/24.
 //
 
-//import UIKit
-//
-//struct FriendListResponse: Codable {
-//    let friendProfile: UIImage
-//    let friendName: String
-//    let friendStatusMessage: String
-//    
-//    func convertToFriendList() -> FriendListModel {
-//        return FriendListModel(friendProfile: self.friendProfile, friendName: self.friendName, friendStatusMessage: self.friendStatusMessage)
-//    }
-//}
+import UIKit
+
+struct FriendListResponse: Codable {
+    let friendProfile: String
+    let friendName: String
+    let friendStatusMessage: String
+    
+    init(friendProfile: String, friendName: String, friendStatusMessage: String) {
+        self.friendProfile = friendProfile
+        self.friendName = friendName
+        self.friendStatusMessage = friendStatusMessage
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case friendProfile
+        case friendName
+        case friendStatusMessage
+    }
+    
+    init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        friendProfile = try values.decode(String.self, forKey: .friendProfile)
+        friendName = try values.decode(String.self, forKey: .friendName)
+        friendStatusMessage = try values.decode(String.self, forKey: .friendStatusMessage)
+    }
+}
--- Kakao-Clone/Kakao-Clone/Presentation/Main/FriendTab/ViewController/FriendViewController.swift
+++ Kakao-Clone/Kakao-Clone/Presentation/Main/FriendTab/ViewController/FriendViewController.swift
@@ -111,6 +111,7 @@ extension FriendViewController {
             } else {
                 for document in querySnapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
+
                 }
             }
         }
