c8177f220d05382936ef84803cdcccd43360adab
kwonjeong <kwonjeong9470@gmail.com>
2023-04-02 22:23:10 +0900

    [Fix] #18 - 주석 삭제

--- Kakao-Clone/Kakao-Clone/Data/DTO/FriendListResponse.swift
+++ Kakao-Clone/Kakao-Clone/Data/DTO/FriendListResponse.swift
@@ -6,38 +6,27 @@
 //
 
 import UIKit
+import Firebase
 
 struct FriendListResponse: Codable {
     let friendProfile: String
     let friendName: String
     let friendStatusMessage: String
     
+    init?(document: QueryDocumentSnapshot) {
+        guard let data = document.data() as? [String: Any],
+              let profile = data["imageURL"] as? String,
+              let message = data["message"] as? String,
+              let name = data["name"] as? String else {
+            return nil
+        }
+        
+        self.friendProfile = profile
+        self.friendName = name
+        self.friendStatusMessage = message
+    }
+    
     func convertToFriendList() -> FriendListModel {
         return FriendListModel(friendProfile: self.friendProfile, friendName: self.friendName, friendStatusMessage: self.friendStatusMessage)
     }
-    
-//    init(friendProfile: String, friendName: String, friendStatusMessage: String) {
-//        self.friendProfile = friendProfile
-//        self.friendName = friendName
-//        self.friendStatusMessage = friendStatusMessage
-//    }
-//
-//    private enum CodingKeys: String, CodingKey {
-//        case friendProfile
-//        case friendName
-//        case friendStatusMessage
-//    }
-//
-//    init(from decoder: Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        friendProfile = try values.decode(String.self, forKey: .friendProfile)
-//        friendName = try values.decode(String.self, forKey: .friendName)
-//        friendStatusMessage = try values.decode(String.self, forKey: .friendStatusMessage)
-//    }
-//
-//    func convertToFriend() -> FriendListModel {
-//        print(self.friendProfile)
-//        print(self.friendName)
-//        return FriendListModel(friendProfile: self.friendProfile, friendName: self.friendName, friendStatusMessage: self.friendStatusMessage)
-//    }
 }
